5f6153ab020f7c35d178c9214ea613cd771b2329
aman2000raj <amankayshap10@gmail.com>
2024-05-10 14:09:08 +0530

    Add completeTask and RemoveTask functionality

--- src/component/ToDoItems.js
+++ src/component/ToDoItems.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-function ToDoItems({ Item }) {
+function ToDoItems({ Item, completeItem }) {
   return (
-    <li>
-      <span>{Item} </span>
+    <li onClick={completeItem}>
+      <span
+        style={{ textDecoration: Item.isCompleted ? 'line-through' : 'none' }}
+      >
+        {Item.value}
+      </span>
       <input type='checkbox'></input>
     </li>
   );
--- src/component/ToDoList.js
+++ src/component/ToDoList.js
@@ -10,23 +10,45 @@ function ToDoList() {
     'Take a shower',
   ];
 
-  const [items, setItems] = useState(initialItems);
+  const initialItemsData = initialItems.map((item) => ({
+    value: item,
+    isCompleted: false,
+  }));
+
+  const [items, setItems] = useState(initialItemsData);
+
+  const completeTask = (index) => {
+    const newItems = [...items];
+    newItems[index].isCompleted = !newItems[index].isCompleted;
+    setItems(newItems);
+  };
+
+  const clearCompleteTask = () => {
+    const newiIncompleteItems = items.filter((item) => !item.isCompleted);
+    setItems(newiIncompleteItems);
+  };
 
   const handleEmptyItem = () => {
     setItems([]);
   };
+
+  const isAnyItemCompleted = items.some((item) => item.isCompleted);
 
   return (
     <>
       <ul>
         {items.length === 0 && <li>Nothing to do buddy. Sleep!</li>}
         {items.map((item, idx) => (
-          <ToDoItems key={idx} Item={item} />
+          <ToDoItems
+            key={idx}
+            Item={item}
+            completeItem={() => completeTask(idx)}
+          />
         ))}
       </ul>
       <button
         type='button'
-        onClick={handleEmptyItem}
+        onClick={isAnyItemCompleted ? clearCompleteTask : handleEmptyItem}
         disabled={items.length === 0}
       >
         Empty
